83827f4bd51472e180ecf7c1d4dbe1d76f82435b
NGOGA Adolphe <adolphengoga@gmail.com>
2024-09-05 17:25:41 +0200

    creating a module to perform subtraction

--- src/calculator.java
+++ src/calculator.java
@@ -2,4 +2,8 @@ public class calculator {
     public static float addition(float a, float b) {
         return a + b;
     }
+
+    public static float subtraction(float a, float b) {
+        return a - b;
+    }
 }
