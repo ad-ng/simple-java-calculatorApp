90cb0d8bbc7dfc9f0a4b1d71126c6ad6b7608943
NGOGA Adolphe <adolphengoga@gmail.com>
2024-09-05 16:48:27 +0200

    prompting user to tell us which operation he about to perform

--- src/App.java
+++ src/App.java
@@ -3,7 +3,13 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("welcome to the adn calculator app");
+        System.out.println("type: 1 for addition");
+        System.out.println("type: 2 for subtraction");
+        System.out.println("type: 3 for multiplication");
+        System.out.println("type: 4 for division");
+        String sign = scanner.nextLine();
+        System.out.println("you have typed:" +sign);
         
     }
 }
