cff28941d9975fe115378821c4275acedf4a9bd0
NGOGA Adolphe <adolphengoga@gmail.com>
2024-09-10 22:33:34 +0200

    creating a function to perform division

--- src/calculator.java
+++ src/calculator.java
@@ -10,4 +10,8 @@ public class calculator {
     public static float multiplication(float a, float b) {
         return a * b;
     }
+
+    public static float division(float a, float b) {
+        return a / b;
+    }
 }
